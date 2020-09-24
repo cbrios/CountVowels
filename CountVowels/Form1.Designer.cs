96bef561753b91d5c56a5c23b4946dd0d1030e01
Chavela Rios <mv8865pr@go.minnstate.edu>
2020-09-23 22:59:21 -0500

    Add project files.

---
+++ CountVowels/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace CountVowels
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInstruct = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnCount = new System.Windows.Forms.Button();
+            this.txtCount = new System.Windows.Forms.TextBox();
+            this.lblVowels = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblInstruct
+            // 
+            this.lblInstruct.AutoSize = true;
+            this.lblInstruct.Location = new System.Drawing.Point(13, 13);
+            this.lblInstruct.Name = "lblInstruct";
+            this.lblInstruct.Size = new System.Drawing.Size(495, 13);
+            this.lblInstruct.TabIndex = 0;
+            this.lblInstruct.Text = "Enter a paragraph of text. All vowels, upper and lowercase will be counted. Y is " +
+    "not considered a vowel.";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(16, 60);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(271, 134);
+            this.txtInput.TabIndex = 1;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // btnCount
+            // 
+            this.btnCount.Location = new System.Drawing.Point(12, 200);
+            this.btnCount.Name = "btnCount";
+            this.btnCount.Size = new System.Drawing.Size(75, 37);
+            this.btnCount.TabIndex = 2;
+            this.btnCount.Text = "Count";
+            this.btnCount.UseVisualStyleBackColor = true;
+            this.btnCount.Click += new System.EventHandler(this.btnCount_Click);
+            // 
+            // txtCount
+            // 
+            this.txtCount.Location = new System.Drawing.Point(93, 209);
+            this.txtCount.Name = "txtCount";
+            this.txtCount.ReadOnly = true;
+            this.txtCount.Size = new System.Drawing.Size(61, 20);
+            this.txtCount.TabIndex = 3;
+            // 
+            // lblVowels
+            // 
+            this.lblVowels.AutoSize = true;
+            this.lblVowels.Location = new System.Drawing.Point(160, 212);
+            this.lblVowels.Name = "lblVowels";
+            this.lblVowels.Size = new System.Drawing.Size(40, 13);
+            this.lblVowels.TabIndex = 4;
+            this.lblVowels.Text = "vowels";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 249);
+            this.Controls.Add(this.lblVowels);
+            this.Controls.Add(this.txtCount);
+            this.Controls.Add(this.btnCount);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblInstruct);
+            this.Name = "frmMain";
+            this.Text = "Count Vowels";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInstruct;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnCount;
+        private System.Windows.Forms.TextBox txtCount;
+        private System.Windows.Forms.Label lblVowels;
+    }
+}
+
---
+++ CountVowels/Form1.cs
@@ -0,0 +1,78 @@
+﻿// Author : Chavela Rios
+// Date   : 2020-09-23
+// Write a GUI program using WPF named CountVowels that accepts a phrase from the user and counts the number of 
+//vowels in the phrase. For this exercise, count both uppercase and lowercase vowels, but do not consider a y to be a vowel.
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CountVowels
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        // Button event
+        private void btnCount_Click(object sender, EventArgs e)
+        {
+            // Variable to declare human input
+            String input;
+
+            // Assigning input from human
+            input = txtInput.Text;
+
+            // Counts the vowels
+            getVowels(input);
+        }
+
+        // When window is loaded event
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            // Makes sure TextBoxs are blank
+            txtInput.Text = "";
+            txtCount.Text = "";
+        }
+
+        // Text changed in textbox event
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+            // Makes sure TextBox is blank if entering another input
+            txtCount.Text = "";
+        }
+
+        // Obtains number of vowels in human input
+        private void getVowels(String input)
+        {
+            // Declare and reset vowel count
+            int v = 0;
+
+            // Loop that sorts through string length and adds to total if matches to vowels listed below
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == 'a')
+                    v++;
+                if (input[i] == 'e')
+                    v++;
+                if (input[i] == 'i')
+                    v++;
+                if (input[i] == 'o')
+                    v++;
+                if (input[i] == 'u')
+                    v++;
+            }
+
+            // Displays total in textbox
+            txtCount.Text = v.ToString();
+        }
+    }
+}
